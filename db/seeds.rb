e7bfda3404143d0b660b85bb7e60999b6cac562d
ammarshah <ammarshah123@gmail.com>
2015-05-11 16:43:43 +0500

    Added Rolify.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
-# Environment variables (ENV['...']) can be set in the file config/application.yml.
-# See http://railsapps.github.io/rails-environment-variables.html
+## Delete all roles ##
+Role.destroy_all
+## Restart column id sequence to start with 1 ##
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE roles_id_seq RESTART WITH 1")
+
+## Add default roles ##
+Role.create(
+	name: "admin"
+	)
+Role.create(
+	name: "restaurant_owner"
+	)
