82d0d84aaafdb18be6ea1d9637b3b8b1c74303b4
ammarshah <ammarshah123@gmail.com>
2015-05-18 12:38:53 +0500

    SMTP authentication settings changes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,14 +74,14 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_apikey
+    user_name: "attribesolution@gmail.com",
+    password: "Attrib3s"
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => "gmail.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
