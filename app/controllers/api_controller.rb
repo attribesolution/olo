2fc45fb81fc9f97aa2d122402827c96e2240abfa
ammarshah <ammarshah123@gmail.com>
2015-05-26 22:04:29 +0500

    Device Mapping API, done.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,11 @@
 class ApiController < ApplicationController
+	protect_from_forgery with: :null_session
+  
 	def authenticate_api_request
     api_key = request.headers["HTTP_AUTHORIZATION"]
     @user = User.where(api_key: api_key).first
   
-    render :json => { :status => 401, message: "Invalid api key." } if @user.nil?
+    render :json => { :status => 401, message: "API key is required." } if api_key.blank?
+    render :json => { :status => 401, message: "Invalid API key." } if @user.nil?
   end
 end
