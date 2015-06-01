119c2c740f08b1c83565a717a226e64bb9e8a032
Mohsin Ali <mohammadmohsin@gmail.com>
2015-06-01 05:22:32 +0500

    Show orders on Dashboard

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,3 +1,7 @@
 class VisitorsController < ApplicationController
 	before_action :authenticate_user!
+
+	def index
+    @orders = Order.all
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,4 @@
 class OrderDetail < ActiveRecord::Base
+	belongs_to :order
+  belongs_to :menu
 end
