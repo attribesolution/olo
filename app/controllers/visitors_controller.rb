db1c2dbd15352e53a8bdc719012546e64656e97d
ammarshah <ammarshah123@gmail.com>
2015-06-05 17:40:08 +0500

    Show latest order first on dashboard.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,6 @@ class VisitorsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-    @orders = Order.by_user current_user.id
+    @orders = Order.by_user(current_user.id).order(created_at: :desc)
   end
 end
