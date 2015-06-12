332d1bc708adee2a79d230ef0de3fcc0add262c1
Mohsin Ali <mohammadmohsin@gmail.com>
2015-06-12 16:51:10 +0500

    Minor fixing.

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::DevicesController < ApiController
   before_action :check_required_params, :validates_uniqueness
+  skip_before_action :authenticate_api_request
 
   def create
     device = DeviceTableMapping.new(device_id: @device_id, device_name: @device_name, restaurant_owner_id: @user.id)
     begin
       device.save!
-      render :json => { message: "Device mapped successfully.", :status => 200 }
+      render :json => { message: "Device mapped successfully.", key: @user.api_key, :status => 200 }
     rescue => e
       render :json => { message: e.message, :status => 501 }
     end
@@ -26,10 +27,10 @@ class Api::V1::DevicesController < ApiController
     @device_name = params[:device_name]
     @passcode = params[:passcode]
 
-    @passcode = User.where(passcode: @passcode)
-    return render :json => { message: "Invalid Passcode", :status => 422 } unless @passcode.any?
+    @user = User.where(passcode: @passcode).first
+    return render :json => { message: "Invalid Passcode", :status => 422 } if @user.nil?
 
-    conflicted_device_id = DeviceTableMapping.where(device_id: @device_id, restaurant_owner_id: @passcode.id)
+    conflicted_device_id = DeviceTableMapping.where(device_id: @device_id, restaurant_owner_id: @user.id)
     return render :json => { message: "Device ID already exists.", :status => 422 } if conflicted_device_id.any?
 
     conflicted_device_name_of_this_restaurant_owner = DeviceTableMapping.where("lower(device_name) = ? and restaurant_owner_id= ?", @device_name.downcase, @user.id)
