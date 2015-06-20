e9473f5ca5390a2f9f21721605b19566e123b1e2
Mohsin Ali <mohammadmohsin@gmail.com>
2015-06-20 15:53:22 +0500

    Link to destroy categories.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :restaurant_owner, :class_name => "User", :foreign_key => "restaurant_owner_id"
-  has_many :menus
+  has_many :menus, dependent: :destroy
 
   mount_uploader :image, CategoryImageUploader
 
