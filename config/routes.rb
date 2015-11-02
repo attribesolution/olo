4658a05a68de4df4dfe226b36d07d8ee415077ae
Yahya Waseem <yahyawasim@live.com>
2015-11-02 17:46:35 +0500

    routes.rb fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   resources :device_table_mappings
   resources :menus do
+    resources :menu_images, :only => [:destroy]
     collection do
       delete 'destroy_multiple'
-    resources :menu_images, :only => [:destroy]
     end
   end
 
