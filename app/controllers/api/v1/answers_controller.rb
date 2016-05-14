a217d68044be1b6237e708d717fe671165373250
Mohammad Umer <mohammadumer1991@gmail.com>
2016-05-14 18:17:57 +0500

    Validate answer array through json

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::AnswersController < ApiController
 	def create
-		survey = Survey.create(phone_no: params["phone_no"], order_id: params["order_id"])
 		answers = params["answers"]
-		answers.each do |answer|
-    	Answer.create(answer: answer["answer"], question_id: answer["question_id"], survey_id: survey.id) 
+		if answers.nil?
+			render :json => { messgae: "Please Insert Answers"}
+		else
+			survey = Survey.create(phone_no: params["phone_no"], order_id: params["order_id"])
+			answers.each do |answer|
+	    	Answer.create(answer: answer["answer"], question_id: answer["question_id"], survey_id: survey.id) 
+			end
+			render :json => { message: "Answers saved successfully.", :status => 200 }
 		end
-		render :json => { message: "Answers saved successfully.", :status => 200 }
 	end
 end
