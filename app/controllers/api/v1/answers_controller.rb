4bf96bf1b1b05ecd0964a68b815d7663320552df
Mohammad Umer <mohammadumer1991@gmail.com>
2016-05-14 18:01:04 +0500

    Change made in answers api

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::AnswersController < ApiController
 	def create
-		@survey = Survey.create(phone_no: params["phone_no"], order_id: params["order_id"])
-		@answer = Answer.new(answer: params[:answer], question_id: params[:question_id], phone_no: params[:phone_no], order_id: params[:order_id]) 
-		@answer.survey = @survey
-		@answer.save
-
+		survey = Survey.create(phone_no: params["phone_no"], order_id: params["order_id"])
+		answers = params["answers"]
+		answers.each do |answer|
+    	Answer.create(answer: answer["answer"], question_id: answer["question_id"], survey_id: survey.id) 
+		end
 		render :json => { message: "Answers saved successfully.", :status => 200 }
 	end
 end
