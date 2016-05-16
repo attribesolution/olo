eaf86018d23f19122fda437e05fc73b4bb63a5b3
Mohammad Umer <mohammadumer1991@gmail.com>
2016-05-16 12:27:25 +0500

    Exception handling in answers api

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -2,13 +2,18 @@ class Api::V1::AnswersController < ApiController
 	def create
 		answers = params["answers"]
 		if answers.nil?
-			render :json => { messgae: "Please Insert Answers"}
+			render :json => { messgae: "Atleast one answer is required."}
 		else
-			survey = Survey.create(phone_no: params["phone_no"], order_id: params["order_id"])
-			answers.each do |answer|
-	    	Answer.create(answer: answer["answer"], question_id: answer["question_id"], survey_id: survey.id) 
-			end
-			render :json => { message: "Answers saved successfully.", :status => 200 }
+      begin
+  			survey = Survey.create(phone_no: params["phone_no"], order_id: params["order_id"])
+  			answers.each do |answer|
+  	    	Answer.create(answer: answer["answer"], question_id: answer["question_id"], survey_id: survey.id) 
+  			end
+  			render :json => { message: "Answers saved successfully.", :status => 200 }
+      rescue Exception => e
+        render :json => { message: e.message, status: 201 }
+      end
 		end
 	end
+  
 end
