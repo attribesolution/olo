86a849a0556407b7fb764f71ab597d8307e1ca85
Mohammad Umer <mohammadumer1991@gmail.com>
2016-05-16 19:19:56 +0500

    Exception handling in questions api

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AnswersController < ApiController
 	def create
 		answers = params["answers"]
 		if answers.nil?
-			render :json => { messgae: "Atleast one answer is required."}
+			render :json => { messgae: "Atleast one answer is required.", status => 200 }
 		else
       begin
   			survey = Survey.create(phone_no: params["phone_no"], order_id: params["order_id"])
--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::QuestionsController < ApiController
 	def index
 		@questions = @user.questions.order('created_at DESC')
+		if @questions.blank?
+			return render :json => { messgae: "You have not added any question", status => 200 }
+		end
 	end
 end
