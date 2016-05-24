ef1a4a5579586906b5e9fb8d88ada5d99eb2e3fb
Ammar Shah <ammarshah123@gmail.com>
2016-05-24 18:53:14 +0500

    Import all data from sync API

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -33,11 +33,28 @@ class SettingsController < ApplicationController
       response = HTTParty.get(Rails.application.secrets.dmenu_domain_address + '/api/v1/user/get_api_key?passcode=' + params[:passcode])
       if response["api_key"].present?
         sync_response = HTTParty.get(Rails.application.secrets.dmenu_domain_address + '/api/v1/categories/sync', headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json', 'Authorization' => response["api_key"] })
+        import_dmenu_data(sync_response)
       end
+      flash[:notice] = "Your DMenu account data has been synced successfully."
     rescue Exception => e
       flash[:alert] = e.message
     end
     redirect_to root_path
+  end
+
+  def import_dmenu_data(sync_response)
+    Category.where(restaurant_owner_id: current_user.id).destroy_all # this will destroy all categories and its dependencies
+    sync_response.each do |category|
+      new_category = Category.create(name: category["name"], image: category["image"], restaurant_owner_id: current_user.id)
+      
+      category["menus"].each do |menu|
+        new_menu = Menu.create(name: menu["name"], price: menu["price"], description: menu["description"], category_id: new_category.id, approved: menu["approved"], restaurant_owner_id: current_user.id)
+        
+        menu["images"].each do |menu_image|
+          new_menu_image = MenuImage.create(image: menu_image["url"], menu_id: new_menu.id)
+        end
+      end
+    end
   end
 
 end
