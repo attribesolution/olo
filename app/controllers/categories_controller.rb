34ddbf90922e3e8b125261cd1fff6e56ea6ccf99
Mohammad Umer <mohammadumer1991@gmail.com>
2016-06-03 20:46:23 +0500

    Create Update Delete functionality remove from controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,82 +13,9 @@ class CategoriesController < ApplicationController
   def show
   end
 
-  # GET /categories/new
-  def new
-    @category = Category.new
-  end
-
-  # GET /categories/1/edit
-  def edit
-  end
-
-  # POST /categories
-  # POST /categories.json
-  def create
-    @category = Category.new(category_params)
-    @category.restaurant_owner_id = current_user.id
-    @category.image = category_params[:image]
-    
-    respond_to do |format|
-      if @category.save
-        current_user.device_table_mappings.each do |device|
-          device.updated = false
-        end
-        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        current_user.device_table_mappings.each do |device|
-          device.updated = false
-        end
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1
-  # DELETE /categories/1.json
-  def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  def destroy_multiple
-    if params[:categories_ids]
-      Category.destroy(params[:categories_ids])
-    end
-    
-    respond_to do |format|
-      format.html { redirect_to categories_path}
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def category_params
-      params.require(:category).permit(:name, :image, :remove_image, :image_cache, :dirty, :parent_id)
     end
 end
--- app/controllers/menu_images_controller.rb
+++ app/controllers/menu_images_controller.rb
@@ -1,8 +1,2 @@
 class MenuImagesController < ApplicationController
-  def destroy
-    @menu = Menu.find(params[:menu_id])
-    @photo = @menu.menu_images.find(params[:id])
-    @photo.destroy
-    redirect_to menu_path(@menu)
-  end
 end
