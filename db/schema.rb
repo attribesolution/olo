d55f91d021fd901115cb22c926c4a1df361d4747
salman <salmanqureshi3029@gmail.com>
2016-08-12 15:31:20 +0500

    columsn added to reservation, create reservation is yet to be properly implemented

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811131014) do
+ActiveRecord::Schema.define(version: 20160812102022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,9 @@ ActiveRecord::Schema.define(version: 20160811131014) do
     t.integer  "restaurant_owner_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "name"
+    t.string   "email"
+    t.integer  "branch_id"
   end
 
   create_table "roles", force: :cascade do |t|
