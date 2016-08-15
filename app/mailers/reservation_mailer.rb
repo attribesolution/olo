a886164fdddaea556cdaf7842eaedb1c973d8340
salman <salmanqureshi3029@gmail.com>
2016-08-15 20:11:59 +0500

    done with emails!

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -5,6 +5,8 @@ class ReservationMailer < ApplicationMailer
   end
   def email_to_restaurant(reservation)
   	@reservation = reservation
+  	
+  	@time = Time.at(@reservation.time)
   	mail(to: 'salman.jamilsst@gmail.com', subject: 'This is for restaurant call center')
   end
 
