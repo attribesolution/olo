fa6f3e74c09d13fe19824a5a55c92bc25b1fbc8a
Mohammad Umer <mohammadumer1991@gmail.com>
2017-06-14 17:56:17 +0500

    Email is being sent to customer now after reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,7 +2,7 @@ class ReservationsController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		if params[:lastReservationId]
+	if params[:lastReservationId]
       @reservations = Reservation.by_restaurant(current_user.id).from_reservation(params[:lastReservationId]).page(params[:page]).per(10).order(created_at: :desc)
     else
 	    @reservations = Reservation.by_restaurant(current_user.id).page(params[:page]).per(10).order(created_at: :desc)
